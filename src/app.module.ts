4bf880264df75f93316d29f60287e22d41204e27
Diego Pagini <diego.pagini@neoris.com>
2022-12-30 14:25:14 +0100

    graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      // playground: false
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
